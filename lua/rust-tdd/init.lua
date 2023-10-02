7d1918803a2f2b9b462216e0cd25d60639e7bd4a
Matthias Gruen <gruen.matthias@gmx.net>
2023-10-02 16:27:31 +0200

    Check if file has tests

--- lua/rust-tdd/init.lua
+++ lua/rust-tdd/init.lua
@@ -1,15 +1,39 @@
 local M = {}
 
-function M.setup()
-   local ns = vim.api.nvim_create_namespace("ic0r")
+local function has_tests(bufnr)
+   local lang_tree = vim.treesitter.get_parser(bufnr, "rust")
+   local syntax_tree = lang_tree:parse()
+   local root = syntax_tree[1]:root()
 
+   local query = vim.treesitter.query.parse("rust", [[
+      (attribute_item
+         (attribute
+            (identifier) @attribute_name (#eq? @attribute_name "cfg")
+            (token_tree
+               (identifier) @attribute_argument (#eq? @attribute_argument "test")
+            )
+         )
+      )
+   ]])
+
+   local id,_node,_metadata = query:iter_captures(root, bufnr, 0, -1)()
+   return id ~= nil
+end
+
+function M.setup()
    vim.api.nvim_create_autocmd("BufWritePost", {
-      group = vim.api.nvim_create_augroup("Test", { clear = true }),
+      group = vim.api.nvim_create_augroup("ic0r", { clear = true }),
       pattern = "*.rs",
       callback = function()
+         local ns = vim.api.nvim_create_namespace("ic0r")
          local source_buf = vim.api.nvim_get_current_buf()
          vim.api.nvim_buf_clear_namespace(source_buf, ns, 0, -1)
          vim.api.nvim_buf_clear_namespace(source_buf, ns, 0, -1)
+
+         if not has_tests(source_buf) then
+            return
+         end
+  
          local buf_lines = vim.api.nvim_buf_get_lines(source_buf, 0, -1, false)
          local fails = {}
          vim.fn.jobstart({ "cargo", "test" }, {
@@ -25,7 +49,7 @@ function M.setup()
                         tmp = "❌"
                      end
                   else
-                     tmp = "✅ "
+                     tmp = "✅"
                      success = true
                   end
                   if tmp then
@@ -67,6 +91,8 @@ function M.setup()
                end
                --vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, data)
             end,
+
+            -- "cargo test" exited
             on_exit = function()
                vim.diagnostic.set(ns, source_buf, fails, {})
             end
